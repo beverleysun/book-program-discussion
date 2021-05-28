72c62dd3660b14715d5fe385b7fa542aabf00ee5
beverleysun <beverleysun0205@gmail.com>
2021-05-28 21:40:29 +1200

    animate pop-up on back to top button

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ document
 document.querySelector("#close-form").addEventListener("click", hideForm);
 document.querySelector(".overlay").addEventListener("click", hideForm);
 document.querySelector(".back-to-top").addEventListener("click", scrollToTop);
+document
+  .querySelector(".back-to-top")
+  .addEventListener("mouseenter", toTopButtonBigger);
+document
+  .querySelector(".back-to-top")
+  .addEventListener("mouseleave", toTopButtonSmaller);
 window.addEventListener("scroll", onScroll);
 onScroll();
+
+function toTopButtonBigger() {
+  let button = document.querySelector(".back-to-top");
+  button.style.transition = "ease-out 0.3s";
+  button.style.transform = "scale(1.1)";
+}
+
+function toTopButtonSmaller() {
+  let button = document.querySelector(".back-to-top");
+
+  button.style.transform = "scale(1)";
+  button.style.transition = "ease-out 0.1s";
+}
 
 function showForm() {
   let overlay = document.querySelector(".overlay");
@@ -30,9 +49,8 @@ function scrollToTop() {
 function onScroll() {
   let button = document.querySelector(".back-to-top");
   if (window.scrollY > (3 * window.innerHeight) / 4) {
-    console.log("h");
-    button.style.display = "flex";
+    button.style.transform = "scale(1)";
   } else {
-    button.style.display = "none";
+    button.style.transform = "scale(0)";
   }
 }
